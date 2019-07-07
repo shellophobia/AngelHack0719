703ab880ee1fab9feb86335a2a41e3319d2c3165
Suman Anand <suman.anand@grabtaxi.com>
2019-07-07 11:04:58 +0800

    add badges

--- angelhack0719/src/TopicPods.jsx
+++ angelhack0719/src/TopicPods.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './TopicPods.css';
+import Badge from '@material-ui/core/Badge';
 import {times} from 'lodash';
 import ChatBox from './ChatBox';
 
 const TopicPods = (props) => {
   const [podOpen, setPodOpen] = React.useState(false);
-
   const handlePodClick = (index) => {
     setPodOpen(true);
   };
@@ -14,7 +14,7 @@ const TopicPods = (props) => {
     <div style={{position: 'relative'}}>
       {
         times(10).map((val, index) => (
-          <div className={`pod pod${(index+1)}`} key={`pod${index}`} onClick={() => handlePodClick(index)} />
+          <Badge color="secondary" className={`pod pod${(index+1)}`} invisible={index%2==0} onClick={() => handlePodClick(index)} badgeContent={index*10}><div key={`pod${index}`}/></Badge>
         ))
       }
       {podOpen &&
